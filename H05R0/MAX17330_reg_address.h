7f55ebfb5a240ed036297e464ec9462ace0b9af7
Mohamad Khatib <116002861+mohamad1999khatib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@users.noreply.github.com>
2024-02-14 12:40:19 +0300

    Adding:MAX17330_reg_address.h

---
+++ H05R0/MAX17330_reg_address.h
@@ -0,0 +1,8 @@
+/*
+ BitzOS (BOS) V0.3.0 - Copyright (C) 2017-2024 Hexabitz
+ All rights reserved
+
+ File Name     : MAX17330_reg_address.h
+ Description   :  Header file of register battery charger/gauge (MAX17330).
+
+ */
